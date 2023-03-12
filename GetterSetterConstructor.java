6f5e4a843a77f415a056510b5f92da6859547fda
Wasif <wzf8801@gmail.com>
2023-03-12 08:56:57 +0600

    master branch

---
+++ GetterSetterConstructor.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hall.suppliance.complaint;
+
+/**
+ *
+ * @author Wasif
+ */
+public class GetterSetter {
+    private int roll;
+    private String name;
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public GetterSetter(int roll, String name) {
+        this.roll = roll;
+        this.name = name;
+    }
+    
+    
+}
