2e1a2d9b1e4fbc73cda9005e51ae28a582a8ab3b
Wasif <wzf8801@gmail.com>
2023-03-12 09:08:09 +0600

    aaaa

--- GetterSetterConstructor.java
+++ GetterSetterConstructor.java
@@ -1,3 +1,4 @@
+//aaaaa
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
